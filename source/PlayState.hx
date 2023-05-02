d3a6806291f95d2714e5af125ec328330f766486
HMFNF <51531073+hoaanhmine@users.noreply.github.com>
2023-05-02 11:39:51 +0700

    update source/PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2426,7 +2426,7 @@ class PlayState extends MusicBeatState
 
 	public function updateScore(miss:Bool = false)
 	{
-		scoreTxt.text = 'Score: ' + songScore
+		scoreTxt.text = 'HMFNF port - Score: ' + songScore
 		+ ' | Misses: ' + songMisses
 		+ ' | Rating: ' + ratingName
 		+ (ratingName != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
